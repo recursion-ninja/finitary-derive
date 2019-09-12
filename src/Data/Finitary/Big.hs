3d720dedc172be913e9682b0499d2320f4af10c8
Koz Ross <koz.ross@retro-freedom.nz>
2019-09-13 08:54:08 +1200

    Add Big, add NFData, Data instances, create general Binary instance for any Finitary

---
+++ src/Data/Finitary/Big.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+
+module Data.Finitary.Big where
+
+import Control.Applicative (Alternative(..))
+import Data.Bool (bool)
+import Numeric.Natural (Natural)
+import GHC.Generics (Generic)
+import Type.Reflection (Typeable)
+import Data.Finitary (Finitary(..))
+import Data.Binary (Binary(..))
+import GHC.TypeNats
+import Data.Proxy (Proxy(..))
+import Data.Data (Data)
+import Control.DeepSeq (NFData)
+
+newtype Big a = Big { reduce :: a }
+  deriving (Eq, Ord, Bounded, Generic, Show, Read, Typeable, Data) 
+
+instance (NFData a) => NFData (Big a)
+
+instance (Finitary a) => Finitary (Big a)
+
+instance (Finitary a) => Binary (Big a) where
+  {-# INLINE get #-}
+  get = do card <- get @Natural
+           ix <- get @Natural
+           decodeWith card ix
+  {-# INLINE put #-}
+  put (Big x) = do let ix = fromIntegral @_ @Natural . toFinite $ x
+                   let card = fromIntegral @_ @Natural . subtract 1 . natVal @(Cardinality a) $ Proxy
+                   put card >> put ix
+
+-- Helpers
+
+{-# INLINE decodeWith #-}
+decodeWith :: (Alternative f, Integral n, Finitary a) => n -> n -> f (Big a)
+decodeWith card ix = bool empty (pure . Big . fromFinite . fromIntegral $ ix) (ix <= card)
--- src/Data/Finitary/PackInto.hs
+++ src/Data/Finitary/PackInto.hs
@@ -9,6 +9,7 @@
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE DeriveDataTypeable #-}
 
 module Data.Finitary.PackInto 
 (
@@ -28,6 +29,8 @@ import Foreign.Storable (Storable(..))
 import Foreign.Ptr (castPtr)
 import Type.Reflection (Typeable)
 import Data.Binary (Binary(..))
+import Data.Data (Data)
+import Control.DeepSeq (NFData)
 
 import qualified Data.Vector.Unboxed as VU
 import qualified Data.Vector.Generic as VG
@@ -35,7 +38,9 @@ import qualified Data.Vector.Generic.Mutable as VGM
 
 -- p is the 'pack into' type
 newtype PackInto (p :: Type) (a :: Type) = PackInto { unpackFrom :: a }
-  deriving (Eq, Ord, Bounded, Generic, Show, Read, Typeable)
+  deriving (Eq, Ord, Bounded, Generic, Show, Read, Typeable, Data)
+
+instance (NFData a) => NFData (PackInto p a)
 
 instance (Finitary a) => Finitary (PackInto p a)
 
